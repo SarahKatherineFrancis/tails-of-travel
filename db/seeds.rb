f17e987d2c1a961237c9802b0e438c862b1336e0
Sarah Francis <sarah.katherinefrancis@gmail.com>
2023-04-18 20:52:54 +0200

    chore: added first name and last name to user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 # Create a user
-user = User.create!(email: 'mail@mail.com', password: '123123', password_confirmation: '123123')
-bot = User.create!(email: 'test@test.com', password: '123123', password_confirmation: '123123')
+user = User.create!(email: 'mail@mail.com', password: '123123', password_confirmation: '123123', first_name: 'Tom', last_name: 'Smith')
+bot = User.create!(email: 'test@test.com', password: '123123', password_confirmation: '123123', first_name: 'Sam', last_name: 'Brown')
 
 # Create some blog posts for the user
 Blog.create!(title: "My Trip to the Beach", content: "I had a great time at the beach with my dog. We played in the
-  waves and walked along the shore.", user: user)
+  waves and walked along the shore.", location: 'Muizenberg Beach, Cape Town', user: user)
 Blog.create!(title: "Hiking in the Mountains", content: "My dog and I went on a challenging hike in the mountains. The
-  views were amazing!", user: user)
+  views were amazing!", location: 'Table Mountain', user: user)
 
 # Create some trips
 Trip.create!(name: "Dog-Friendly Beaches in California", description: "Explore the best dog-friendly beaches in
